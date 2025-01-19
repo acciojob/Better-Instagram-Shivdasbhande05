20e1dc0c00390cba57f09e90524e4c9bdf26cb11
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-19 23:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 //your code here
 const image = document.querySelectorAll(".image");
 
-const draggedElemet = null;
+let draggedElemet = null;
 
 image.forEach((items) => {
 
     // when drag element start
-    items.addEventListener("dragstart",(e) => {
-        //console.log("Drag Start");
+    items.addEventListener("dragstart", (e) => {
+       // console.log("Drag Start");
         draggedElemet = e.target;
     });
 
     // when drag element end triggered this event
     items.addEventListener("dragend", (e) => {
-        //console.log("Drag End");
+       // console.log("Drag End");
         draggedElemet = null;
     });
 
     //allow drop event 
     items.addEventListener("dragover", (e) => {
         e.preventDefault();
-        //console.log("drag over completed");
+       // console.log("drag over completed");
     });
 
     // handle drop event
     items.addEventListener("drop", (e) => {
         e.preventDefault();
-
         if(draggedElemet && draggedElemet !== e.target){
-            const dragImage = draggedElemet.style.backgroundImage;
-            draggedElemet.style.backgroundImage = e.target.style.backgroundImage;
-            e.target.style.backgroundImage = dragImage;
+            let dragImage = draggedElemet.id;
+            draggedElemet.id = e.target.id;
+            e.target.id = dragImage;
+
         }
 
 
-        //console.log("drop success!")
+       // console.log("drop success!")
     });
 });
+
+
 
